619d8bf7639d9d4dd4d48275ce01d78bd64006c4
Omree Gal-Oz <ogaloz@lbl.gov>
2019-11-21 18:00:09 -0800

    DNA Sequence must be in A C T G and must be uppercase

--- lib/cello_util/file_maker.py
+++ lib/cello_util/file_maker.py
@@ -117,7 +117,8 @@ def make_input_file_str(inp_file_list):
     for inp_dict in inp_file_list:
         low_RPU = str(inp_dict['low_RPU'])
         high_RPU = str(inp_dict['high_RPU'])
-        DNA_Sequence = check_DNA_seq(inp_dict['inp_DNA_sequence'])
+        #We make the DNA sequence entirely uppercase
+        DNA_Sequence = check_DNA_seq(inp_dict['inp_DNA_sequence'], True)
         inp_file_str += inp_dict['inp_gene_name'] + ' ' + low_RPU + ' ' + high_RPU + ' ' + DNA_Sequence + '\n'
 
     return inp_file_str
@@ -133,20 +134,23 @@ def make_output_file_str(output_file_list):
 
 
     for out_dict in output_file_list:
-
-        DNA_Sequence = check_DNA_seq(out_dict['out_DNA_sequence'])
+        # We make the DNA sequence entirely upper case
+        DNA_Sequence = check_DNA_seq(out_dict['out_DNA_sequence'], True)
         output_file_str += out_dict['out_gene_name'] + ' ' + DNA_Sequence + '\n'
     
 
     return output_file_str
 
 # Could add conversion to uppercase
-def check_DNA_seq(DNA_Sequence):
+def check_DNA_seq(DNA_Sequence, upper_bool):
     if not isinstance(DNA_Sequence, str):
         raise Exception("Inputted DNA sequence not 'string'.")
 
     for i in range(len(DNA_Sequence)):
         if DNA_Sequence[i] not in ["A","C","T","G", "a","c","t","g"]:
             raise Exception("DNA Sequence for input must only contain A, C, T, or G. Instead it contains: " + DNA_Sequence[i])
-    return DNA_Sequence.upper()
+    if upper_bool == True:
+        return DNA_Sequence.upper()
+    else:
+        return DNA_Sequence
 
